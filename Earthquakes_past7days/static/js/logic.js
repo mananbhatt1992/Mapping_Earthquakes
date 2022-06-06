085098e3b7649491c9565762d52b171cf0fdbfc1
Manan Bhatt <mananbhatt2010@hotmail.com>
2022-06-05 21:12:27 -0400

    Earthquake Layer

--- Earthquakes_past7days/static/js/logic.js
+++ Earthquakes_past7days/static/js/logic.js
@@ -32,8 +32,16 @@ let map = L.map('mapid', {
   zoom: 3,
   layers: [streets]
 });
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
@@ -93,5 +101,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
           onEachFeature: function(feature, layer) {
             layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
           }
-        }).addTo(map);
+        }).addTo(earthquakes);
+        earthquakes.addTo(map);
     });
