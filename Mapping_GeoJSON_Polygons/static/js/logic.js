da6c5df35ea4c308bb966440d4a6b84a991ef085
Manan Bhatt <mananbhatt2010@hotmail.com>
2022-06-05 20:30:59 -0400

    Polygons

---
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -0,0 +1,53 @@
+// Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
+
+
+let torontoHoods = "https://raw.githubusercontent.com/mananbhatt1992/Mapping_Earthquakes/main/torontoNeighborhoods.json"
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+};
+
+let map = L.map('mapid', {
+  center: [43.7, -79.3],
+  zoom: 2,
+  layers: [streets]
+});
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+let myStyle={
+  color:"blue",
+  weight: 1
+};
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data,{
+    style: myStyle,
+    onEachFeature: function(feature,layer) {
+      console.log(layer)
+      layer.bindPopup("<h2> Neighborhood :" + feature.properties.AREA_NAME + "</h2>")
+    }
+  }).addTo(map);
+});
